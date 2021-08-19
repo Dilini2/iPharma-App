56e6ed57fe36c9b99c348776c047cde90448af47
TUF <it19045290@my.sliit.lk>
2021-08-19 19:21:26 +0530

    First commit

---
+++ app/src/main/java/com/example/navigationmenu/BottamNavigation.java
@@ -0,0 +1,20 @@
+package com.example.navigationmenu;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.navigationmenu.databinding.ActivityBottamNavigationBinding;
+
+public class BottamNavigation extends AppCompatActivity {
+
+
+
+
+}
